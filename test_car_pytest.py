0a44cc1d9032934282d224e1920fa0ca2af17a53
shivampandita1997 <shivampandita1997@gmail.com>
2019-10-17 13:57:16 +0530

    sd

--- test_car_pytest.py
+++ test_car_pytest.py
@@ -5,15 +5,7 @@ import pytest
 
 
 
-@pytest.fixture
-def my_car():
-    return Car(50)
-
-
-def test_car_accelerate(my_car):
-    my_car.accelerate()
-    assert my_car.speed == 55
-
-def test_car_brake(my_car):
-    my_car.brake()
-    assert my_car.speed == 45
+def test_car_brake():
+    car = Car(50)
+    car.brake()
+    assert car.speed == 45
