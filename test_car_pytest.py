cf03b2b4c1a6a2ceedc0b3aacfa1f784f6fef31f
shivampandita1997 <shivampandita1997@gmail.com>
2019-10-17 12:41:45 +0530

    dsf

--- test_car_pytest.py
+++ test_car_pytest.py
@@ -1,8 +1,19 @@
 
 from car import Car
 
+import pytest
 
-def test_car_brake():
-    car = Car(50)
-    car.brake()
-    assert car.speed == 45
+
+
+@pytest.fixture
+def my_car():
+    return Car(50)
+
+
+def test_car_accelerate(my_car):
+    my_car.accelerate()
+    assert my_car.speed == 55
+
+def test_car_brake(my_car):
+    my_car.brake()
+    assert my_car.speed == 45
