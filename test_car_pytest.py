dea383cde6cd59e35f0e8b51f37e96ded4640313
shivampandita1997 <shivampandita1997@gmail.com>
2019-10-17 12:32:23 +0530

    ds

--- test_car_pytest.py
+++ test_car_pytest.py
@@ -1,18 +1,8 @@
 
-import pytest
 from car import Car
-speed_data = {45, 50, 55, 100}
 
 
-@pytest.mark.parametrize("speed_brake", speed_data)
-def test_car_brake(speed_brake):
+def test_car_brake():
     car = Car(50)
     car.brake()
-    assert car.speed == speed_brake
-
-
-@pytest.mark.parametrize("speed_accelerate", speed_data)
-def test_car_accelerate(speed_accelerate):
-    car = Car(50)
-    car.accelerate()
-    assert car.speed == speed_accelerate
+    assert car.speed == 45
